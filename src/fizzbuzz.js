fcd7991dd6e381fab65ad0011acf1a871b09d0c5
Ichurri <santiagoiturrivargas04@gmail.com>
2024-11-19 09:52:15 -0400

    Reducir lineas de codigo

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,15 +2,9 @@ function calcularFizzBuzzDeUnNumero(numero) {
   const divisiblePor3 = numero % 3 === 0;
   const divisiblePor5 = numero % 5 === 0;
 
-  if (divisiblePor3 && divisiblePor5) {
-    return "FizzBuzz";
-  }
-  if (divisiblePor3) {
-    return "Fizz";
-  }
-  if (divisiblePor5) {
-    return "Buzz";
-  }
+  if (divisiblePor3 && divisiblePor5) return "FizzBuzz";
+  if (divisiblePor3) return "Fizz";
+  if (divisiblePor5) return "Buzz";
   return numero.toString();
 }
 
