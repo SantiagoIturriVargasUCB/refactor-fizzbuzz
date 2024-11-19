de69d0bcbbc5bad24ae8760d5c05658397def925
Ichurri <santiagoiturrivargas04@gmail.com>
2024-11-19 10:16:25 -0400

    pruebas pasando

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,37 +1,37 @@
-import { generarFizzBuzzDe, fizzbuzz } from "./fizzbuzz";
+import { calcularFizzBuzzDeUnNumero, generarSecuenciaFizzBuzz } from "./fizzbuzz";
 
-describe("Fizzbuzz de un numero", () => {
-  it("Numero sin regla", () => {
-    expect(generarFizzBuzzDe(1)).toEqual("1");
+describe("FizzBuzz de un número", () => {
+  it("Número sin regla", () => {
+    expect(calcularFizzBuzzDeUnNumero(1)).toEqual("1");
   });
 
   it("Regla de Fizz para 3", () => {
-    expect(generarFizzBuzzDe(3)).toEqual("Fizz");
+    expect(calcularFizzBuzzDeUnNumero(3)).toEqual("Fizz");
   });
 
   it("Regla de Buzz para 5", () => {
-    expect(generarFizzBuzzDe(5)).toEqual("Buzz");
+    expect(calcularFizzBuzzDeUnNumero(5)).toEqual("Buzz");
   });
 
-  it("Regla de Fizz para multiplos de 3", () => {
-    expect(generarFizzBuzzDe(6)).toEqual("Fizz");
-    expect(generarFizzBuzzDe(9)).toEqual("Fizz");
+  it("Regla de Fizz para múltiplos de 3", () => {
+    expect(calcularFizzBuzzDeUnNumero(6)).toEqual("Fizz");
+    expect(calcularFizzBuzzDeUnNumero(9)).toEqual("Fizz");
   });
 
-  it("Regla de Buzz para multiplos de 5", () => {
-    expect(generarFizzBuzzDe(10)).toEqual("Buzz");
-    expect(generarFizzBuzzDe(20)).toEqual("Buzz");
+  it("Regla de Buzz para múltiplos de 5", () => {
+    expect(calcularFizzBuzzDeUnNumero(10)).toEqual("Buzz");
+    expect(calcularFizzBuzzDeUnNumero(20)).toEqual("Buzz");
   });
 
-  it("Regla FizzBuzz para multiplos de 3 y 5", () => {
-    expect(generarFizzBuzzDe(15)).toEqual("FizzBuzz");
-    expect(generarFizzBuzzDe(30)).toEqual("FizzBuzz");
+  it("Regla FizzBuzz para múltiplos de 3 y 5", () => {
+    expect(calcularFizzBuzzDeUnNumero(15)).toEqual("FizzBuzz");
+    expect(calcularFizzBuzzDeUnNumero(30)).toEqual("FizzBuzz");
   });
 
-  describe("Fizzbuzz de una secuencia", () => {
-    it("Genera la secuencia hasta el numero especificado", () => {
-      expect(fizzbuzz(3)).toEqual("1, 2, Fizz");
-      expect(fizzbuzz(5)).toEqual("1, 2, Fizz, 4, Buzz");
+  describe("FizzBuzz de una secuencia", () => {
+    it("Genera la secuencia hasta el número especificado", () => {
+      expect(generarSecuenciaFizzBuzz(3)).toEqual("1, 2, Fizz");
+      expect(generarSecuenciaFizzBuzz(5)).toEqual("1, 2, Fizz, 4, Buzz");
     });
   });
 });
